6cb71ec776a9707fd1bd923e478265b3f256be4e
Dima Zverev <deemoonn@gmail.com>
2023-06-02 18:13:07 +0300

    bug fix

---
+++ src/store/reducer/createOrderSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+interface CreateOrderSliceState {
+	category: string
+	title: string
+	description: string
+	price: string
+	date: string
+}
+
+const initialState: CreateOrderSliceState = {
+	category: '',
+	title: '',
+	description: '',
+	price: '',
+	date: ''
+}
+
+export const createOrderSlice = createSlice({
+	name: 'createOrder',
+	initialState,
+	reducers: {
+		setCategory: (state, action) => {
+			state.category = action.payload
+		},
+		setTitle: (state, action) => {
+			state.title = action.payload
+		},
+		setDescription: (state, action) => {
+			state.description = action.payload
+		},
+		setPrice: (state, action) => {
+			state.price = action.payload
+		},
+		setDate: (state, action) => {
+			state.date = action.payload
+		}
+	}
+})
+
+export default createOrderSlice.reducer
+
+export const { setCategory, setDate, setDescription, setTitle, setPrice } = createOrderSlice.actions
